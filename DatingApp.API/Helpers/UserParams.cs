c02705ff38b4a274a004aeff82c3b9fd655bd669
Benjamin Proulx-Mathers <benjaminproulxmathers@gmail.com>
2019-12-07 18:00:59 -0500

    Add users filtering

--- DatingApp.API/Helpers/UserParams.cs
+++ DatingApp.API/Helpers/UserParams.cs
@@ -12,5 +12,10 @@ namespace DatingApp.API.Helpers
       set { defaultPageSize = (value > MaxPageSize) ? MaxPageSize : value; }
     }
 
+    public int UserId { get; set; }
+    public string Gender { get; set; }
+    public int MinAge { get; set; } = 18;
+    public int MaxAge { get; set; } = 99;
+
   }
 }
